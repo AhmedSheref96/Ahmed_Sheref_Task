7881f507dbb63170a7cc3ad21aeffe629c72543c
Ahmed Sheref <ahmedsheef96@gmail.com>
2022-07-28 17:21:35 +0200

    [ADD POST FULL | ADD PRICE REQUEST DESIGN AND API | BUILD SHEETS AS GLOBAL FOR ADD PAGES]

--- app/src/test/java/com/el3asas/ahmed_sheref_task/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/com/el3asas/ahmed_sheref_task/ui/home/HomeViewModelTest.kt
@@ -1,12 +1,22 @@
 package com.el3asas.ahmed_sheref_task.ui.home
 
+import com.el3asas.ahmed_sheref_task.data.di_modules.diLifeCycleAwaredModules
+import com.el3asas.ahmed_sheref_task.data.di_modules.diSingletonModules
 import com.el3asas.ahmed_sheref_task.models.AssociatedDrugItem
 import com.el3asas.ahmed_sheref_task.models.ClassNameItem
 import com.el3asas.ahmed_sheref_task.models.MedicationsClassesItem
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import junit.framework.Assert.assertEquals
 import org.junit.Test
+import javax.inject.Inject
 
+@UninstallModules(diSingletonModules::class, diLifeCycleAwaredModules::class)
+@HiltAndroidTest
 internal class HomeViewModelTest {
+
+    @Inject
+    lateinit var homeViewModel: HomeViewModel
 
     @Test
     fun mappingData() {
@@ -27,7 +37,8 @@ internal class HomeViewModelTest {
             )
         )
 
-        assertEquals("failed", 4, medicineList?.size)
+        assertEquals("failed", 4, homeViewModel.mappingData(medicineList))
+        println("test succedded")
 
     }
 
