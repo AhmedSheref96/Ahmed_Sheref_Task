155723be6823324937377bf5d60a9f3e0899c792
Ahmed Sheref <ahmedsheef96@gmail.com>
2022-07-28 17:25:40 +0200

    [MAPPING DATA]
    This reverts commit 7881f507dbb63170a7cc3ad21aeffe629c72543c.

--- app/src/test/java/com/el3asas/ahmed_sheref_task/ui/home/HomeViewModelTest.kt
+++ app/src/test/java/com/el3asas/ahmed_sheref_task/ui/home/HomeViewModelTest.kt
@@ -1,22 +1,12 @@
 package com.el3asas.ahmed_sheref_task.ui.home
 
-import com.el3asas.ahmed_sheref_task.data.di_modules.diLifeCycleAwaredModules
-import com.el3asas.ahmed_sheref_task.data.di_modules.diSingletonModules
 import com.el3asas.ahmed_sheref_task.models.AssociatedDrugItem
 import com.el3asas.ahmed_sheref_task.models.ClassNameItem
 import com.el3asas.ahmed_sheref_task.models.MedicationsClassesItem
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
 import junit.framework.Assert.assertEquals
 import org.junit.Test
-import javax.inject.Inject
 
-@UninstallModules(diSingletonModules::class, diLifeCycleAwaredModules::class)
-@HiltAndroidTest
 internal class HomeViewModelTest {
-
-    @Inject
-    lateinit var homeViewModel: HomeViewModel
 
     @Test
     fun mappingData() {
@@ -37,8 +27,7 @@ internal class HomeViewModelTest {
             )
         )
 
-        assertEquals("failed", 4, homeViewModel.mappingData(medicineList))
-        println("test succedded")
+        assertEquals("failed", 4, medicineList?.size)
 
     }
 
